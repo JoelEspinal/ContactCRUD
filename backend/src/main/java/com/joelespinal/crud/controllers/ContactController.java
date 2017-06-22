e9fd5d704ea4c16476da7f5ab1d6b5a4c418383c
Joel Abraham Espinal Carrasco <j.espinalcarrasco@gmail.com>
2017-06-22 18:35:48 -0400

    refactor to repositories and add allow cross origin to localhost:4200

--- backend/src/main/java/com/joelespinal/crud/controllers/ContactController.java
+++ backend/src/main/java/com/joelespinal/crud/controllers/ContactController.java
@@ -1,6 +1,10 @@
 package com.joelespinal.crud.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,27 +12,38 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.joelespinal.crud.dao.ContactDao;
 import com.joelespinal.crud.models.Contact;
+import com.joelespinal.crud.repositories.ContactRepository;
 
 
-@RestController	
+@RestController
 public class ContactController {
 
 //	private ContactRepository contactRepositorie;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(value = "/contacts", method=RequestMethod.GET, produces = "application/json")
 	@ResponseBody 
-	public Iterable<Contact> getContacs(){	
-			return contactDao.findAll();
+	public ResponseEntity<Iterable<Contact>> getContatcs(){	
+			
+			return new ResponseEntity<Iterable<Contact>>(contactRepository.findAll(), HttpStatus.OK);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value = "/contact/{id}", method=RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public ResponseEntity<Contact> getContact(@PathVariable("id") Long id){	
+			Contact contact = contactRepository.findOne(id);
+			return new ResponseEntity<Contact>(contact, HttpStatus.OK);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@ResponseBody
 	@RequestMapping(value = "/contact", method = RequestMethod.POST)
 	public String create(@RequestBody Contact contact){
 	//	Contact contact = null;
 		try{
-			contactDao.save(contact);
+			contactRepository.save(contact);
 		}
 		catch(Exception ex){
 			ex.printStackTrace();
@@ -38,19 +53,19 @@ public class ContactController {
 		return "User succesfully created with id = " +  contact.getId();
 	}
 	
-
-	@RequestMapping(value = "/contact/{id}", method = RequestMethod.PUT)
-	public void update(@PathVariable("id") Long id, @RequestBody Contact contact){
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value = "/contact/{id}", method = RequestMethod.PUT, consumes="application/json")
+	public Contact update(@PathVariable("id") Long id, @RequestBody Contact contact){
 		contact.setId(id);
-		contactDao.save(contact);
+		return contactRepository.save(contact);
 	}
 
-	
-	 @RequestMapping(value = "/contact/{id}", method = RequestMethod.DELETE)
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping(value = "/contact/{id}", method = RequestMethod.DELETE)
 	public void delete(@PathVariable("id") Long id){
-		contactDao.delete(id);
+		contactRepository.delete(id);
 	}
 	
 	@Autowired
-	private ContactDao contactDao;
+	private ContactRepository contactRepository;
 }
